5aeba7bb097a1643b94835635dea9ff3301169ae
fraolketema <138703893+fraolketema@users.noreply.github.com>
2023-12-14 22:48:17 +0300

    Update ope_adder.c

--- ope_adder.c
+++ ope_adder.c
@@ -2,7 +2,7 @@
 
 /**
  * sq_add - add first 2 elements of the stack
- * @head: pointer to pointer to first node
+ * @head: pointer to pointer to first node.
  * @counter: line counters
  * Return: none
  */
